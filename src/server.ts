21178e11a241a6033c29f2c5e034981a5793e1c0
r0cc4t0 <guilherme.roccato@outlook.com>
2021-10-02 19:11:50 -0300

    Database Creation

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,7 @@
 import express from 'express';
+import 'reflect-metadata';
+import './database';
 
 const app = express();
-
-app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello, World!' });
-});
-
-app.post('/', (request, response) => {
-  return response.json({ message: 'Data has been saved successfully!' });
-});
 
 app.listen(3000, () => console.log('Server is running on port 3000...'));
