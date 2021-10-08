0ce49f5de834547db5701b7b2e5565d3e364ffdd
r0cc4t0 <guilherme.roccato@outlook.com>
2021-10-07 21:25:05 -0300

    Tag Creation

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import CreateUserController from './controllers/CreateUserController';
+import CreateTagController from './controllers/CreateTagController';
 
 const routes = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 routes.post('/users', createUserController.handle);
+routes.post('/tags', createTagController.handle);
 
 export default routes;
