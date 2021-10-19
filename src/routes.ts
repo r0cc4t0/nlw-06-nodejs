99fcb38f1e580b05b023f37512089f78db085e9a
r0cc4t0 <guilherme.roccato@outlook.com>
2021-10-18 21:36:13 -0300

    Tags Listing

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import CreateTagController from './controllers/CreateTagController';
 import CreateComplimentController from './controllers/CreateComplimentController';
 import ListComplimentsByUserSenderController from './controllers/ListComplimentsByUserSenderController';
 import ListComplimentsByUserReceiverController from './controllers/ListComplimentsByUserReceiverController';
+import ListTagsController from './controllers/ListTagsController';
 import ensureAuthenticated from './middlewares/ensureAuthenticated';
 import ensureAdmin from './middlewares/ensureAdmin';
 
@@ -16,6 +17,7 @@ const createTagController = new CreateTagController();
 const createComplimentController = new CreateComplimentController();
 const listComplimentsByUserSenderController = new ListComplimentsByUserSenderController();
 const listComplimentsByUserReceiverController = new ListComplimentsByUserReceiverController();
+const listTagsController = new ListTagsController();
 
 routes.post('/users', createUserController.handle);
 routes.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
@@ -24,5 +26,6 @@ routes.post('/compliments', ensureAuthenticated, createComplimentController.hand
 
 routes.get('/compliments/user_sender', ensureAuthenticated, listComplimentsByUserSenderController.handle);
 routes.get('/compliments/user_receiver', ensureAuthenticated, listComplimentsByUserReceiverController.handle);
+routes.get('/tags', ensureAuthenticated, listTagsController.handle);
 
 export default routes;
---
+++ src/services/ListTagsService.ts
@@ -0,0 +1,14 @@
+import { getCustomRepository } from 'typeorm';
+import TagsRepository from '../repositories/TagsRepository';
+
+class ListTagsService {
+  async execute() {
+    const tagsRepository = getCustomRepository(TagsRepository);
+
+    const tags = await tagsRepository.find();
+
+    return tags;
+  }
+}
+
+export default ListTagsService;
