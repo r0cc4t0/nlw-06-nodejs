191d265f2cd697a486c256229eee2bf59b405295
r0cc4t0 <guilherme.roccato@outlook.com>
2021-10-19 22:33:12 -0300

    Users Listing

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import CreateComplimentController from './controllers/CreateComplimentController
 import ListComplimentsByUserSenderController from './controllers/ListComplimentsByUserSenderController';
 import ListComplimentsByUserReceiverController from './controllers/ListComplimentsByUserReceiverController';
 import ListTagsController from './controllers/ListTagsController';
+import ListUsersController from './controllers/ListUsersController';
 import ensureAuthenticated from './middlewares/ensureAuthenticated';
 import ensureAdmin from './middlewares/ensureAdmin';
 
@@ -18,6 +19,7 @@ const createComplimentController = new CreateComplimentController();
 const listComplimentsByUserSenderController = new ListComplimentsByUserSenderController();
 const listComplimentsByUserReceiverController = new ListComplimentsByUserReceiverController();
 const listTagsController = new ListTagsController();
+const listUsersController = new ListUsersController();
 
 routes.post('/users', createUserController.handle);
 routes.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
@@ -27,5 +29,6 @@ routes.post('/compliments', ensureAuthenticated, createComplimentController.hand
 routes.get('/compliments/user_sender', ensureAuthenticated, listComplimentsByUserSenderController.handle);
 routes.get('/compliments/user_receiver', ensureAuthenticated, listComplimentsByUserReceiverController.handle);
 routes.get('/tags', ensureAuthenticated, listTagsController.handle);
+routes.get('/users', ensureAuthenticated, ensureAdmin, listUsersController.handle);
 
 export default routes;
